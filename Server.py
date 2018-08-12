b6cebfd502c43c79ccb02092ac50e06f235494b8
Teemu Alonen <teeqqq@gmail.com>
2018-08-12 20:56:00 +0300

    Small changes to the Server

--- Server.py
+++ Server.py
@@ -10,14 +10,14 @@ import sys
 import threading
 
 class DeepSeaAdventureServer():
-    
+
     def __init__(self, host, port):
         self.host = host
         self.port = port
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind((self.host, self.port))
-        
+
     def listen(self):
         threadCounter = 0
         self.sock.listen(5)
@@ -36,7 +36,7 @@ MSG_NOF_PLAYERS = 'MNP'
 MSG_BREAK = 'MBR'
 
 class ClientThread(threading.Thread):
-    
+
     def __init__(self, threadID, client, address, server):
         threading.Thread.__init__(self)
         self.threadID = threadID
@@ -44,34 +44,33 @@ class ClientThread(threading.Thread):
         self.client = client
         self.address = address
         self.server = server
-    
+
     #thread.start() will call run()-method
     def run(self):
         self.onlyListen()
         print('Client thread {} exiting'.format(self.threadID))
-    
+
     def send(self, msg):
         self.client.sendall(msg)
-        
+
     def onlyListen(self):
         while True:
             cmd = self.client.recv(1024)
-            
+
             if cmd == MSG_NOF_PLAYERS:
                 self.send('99')
             elif cmd == MSG_BREAK:
                 break
 
-    
-    
+
 
 if __name__ == '__main__':
-    
+
     PORT = 1234
-    host = socket.gethostbyname(socket.gethostname())
-    
+    host = '192.168.0.102'
+
     print('Starting TeeQ\'s Deep Sea Adventure game server...')
     print('Host: {}, port: {}'.format(host, PORT))
-    
+
     server = DeepSeaAdventureServer(host, PORT)
-    #server.listen()
+    server.listen()
